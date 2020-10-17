80cb018bb58f02ae94a6544d6ffdd1019eafa0ce
amleshkumar <34007047+amleshkumar@users.noreply.github.com>
2020-10-17 00:38:16 -0700

    Add files via upload

---
+++ ML_29_Assignment2.py
@@ -0,0 +1,85 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+#Question No. 1
+
+
+# In[30]:
+
+
+rows = 6
+for i in range(rows):
+    for j in range(i):
+        print("*", end = "")
+    print("")
+for i in range(rows, 0, -1):
+    for j in range(i):
+        print("*", end = "")
+    print("")
+
+
+# In[31]:
+
+
+#Question No. 2
+
+
+# In[32]:
+
+
+print("Enter a string to reverse")
+original_string = input()
+reverse_string = original_string[::-1]
+print("Reverse String:", reverse_string)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
